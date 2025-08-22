0caf6bfb33103c8f4fef42f4d17065b7ea7cb74b
HWANG JI YEONG <134914704+eve1224v@users.noreply.github.com>
2025-08-22 14:01:51 +0900

    [level 1] Title: 덧칠하기, Time: 0.31 ms, Memory: 5.73 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, int m, vector<int> section) {
+    int answer = 1;
+    int tmp=section[0];
+    for(int i=0; i<section.size(); i++){
+        if(section[i]<tmp+m)
+            continue;
+        else{
+            tmp=section[i];
+            answer++;
+        }
+    }
+    return answer;
+}

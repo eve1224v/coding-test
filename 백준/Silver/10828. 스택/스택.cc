26edbc84f7b28bb1b9c7bd790c0c1fa5b32980e6
HWANG JI YEONG <134914704+eve1224v@users.noreply.github.com>
2025-11-04 19:53:03 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    int x;
+    string order;
+    stack<int> s;
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> order;
+        
+        if (order == "push") {
+            cin >> x;
+            s.push(x);
+        }
+        else if (order == "pop") {
+            if (!s.empty()) {
+                cout << s.top() << "\n";
+                s.pop();
+            }
+            else {
+                cout << "-1" << "\n";
+            }
+        }
+        else if (order == "size") {
+            cout << s.size() << "\n";
+        }
+        else if (order == "empty") {
+            if (s.empty())
+                cout << "1" << "\n";
+            else
+                cout << "0" << "\n";
+        }
+        else if (order == "top") {
+            if (!s.empty())
+                cout << s.top() << "\n";
+            else
+                cout << "-1" << "\n";
+        }
+    }
+
+    return 0;
+}

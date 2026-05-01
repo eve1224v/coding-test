5118b935a1c587f117fd6d8b3cf682ecc6a77b7b
HWANG JI YEONG <134914704+eve1224v@users.noreply.github.com>
2026-05-01 15:39:21 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 17.92 ms, Memory: 13.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    
+    if(arr.size()==1){
+        return {-1};
+    }
+    
+    int minVal=*min_element(arr.begin(), arr.end());
+    
+    arr.erase(find(arr.begin(), arr.end(), minVal));
+    
+    return arr;
+}
